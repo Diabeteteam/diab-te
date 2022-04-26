c5facc70a63420b421ef699805b061425675314a
kadama17 <kadama328@gmail.com>
2022-04-26 00:20:20 +0000

    Chart init on project

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { AlertControllerComponent } from '../component/alert-controller/alert-controller.component';
 import { FirebaseServiceService } from '../firebase-service.service';
+
+import { Chart, registerables } from 'chart.js';
+
 const uid = JSON.parse(localStorage.getItem('user')).uid;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
@@ -11,6 +18,12 @@ const uid = JSON.parse(localStorage.getItem('user')).uid;
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  @ViewChild('barCanvas', { static: true }) barCanvas;
+
+  barChart: any;
+  doughnutChart: any;
+  lineChart: any;
+
   userName: string;
   lastTest: any;
   lastTaux: number;
@@ -21,6 +34,28 @@ export class DashboardPage implements OnInit {
     public fireService: FirebaseServiceService,
     private firebaseServiceService: FirebaseServiceService
   ) {}
+
+  ionViewDidEnter() {
+    this.barChartMethod();
+  }
+  barChartMethod() {
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['BJP', 'INC', 'AAP', 'CPI', 'CPI-M', 'NCP'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [1200, 500, 600, 15, 20, 34],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            borderWidth: 3,
+          },
+        ],
+      },
+    });
+  }
+
   totalTest() {
     const data = this.firebaseServiceService.firestore
       .collection('glycemieTest', (ref) => ref.where('uid', '==', uid))
@@ -73,6 +108,7 @@ export class DashboardPage implements OnInit {
 
     await alert.present();
   }
+
   ngOnInit() {
     this.lastGlycemie();
     this.totalTest();
